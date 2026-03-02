4adbbd26bf64c93b650e2b9d910504f3b1ad025f
Fernando <dwk262@gmail.com>
2026-03-02 10:07:05 +0100

    Initial Commit

---
+++ Curso-ios-firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-ios-firebase
+//
+//  Created by Equipo 9 on 2/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-ios-firebase/Curso_ios_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_firebaseApp.swift
+//  Curso-ios-firebase
+//
+//  Created by Equipo 9 on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
